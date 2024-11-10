11b239d01bd718c94b6213858ee78d89c5713ae2
MKstii <101364255+MKstii@users.noreply.github.com>
2024-11-10 16:16:49 +0500

    Revert "выполнил 1лб"
    This reverts commit 3e618a3ea6c612130d2d34769c7edd82f8bf3072.

--- Marmalade.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Marmalade extends Product{
-    private float $weight;
-
-    public function __construct(string $name, string $description, float $price, float $weight) {
-        parent::__construct($name, $description, $price);
-        $this->weight = $weight;
-    }
-}
